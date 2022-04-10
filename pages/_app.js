60da4cdc977427d1e2f633d252755b2680678fdb
pawan-kumar-dev <pawan.kv612@gmail.com>
2022-04-10 19:49:52 +0530

    env fixed

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,6 @@ import '../styles/globals.css'
 import { MoralisProvider } from 'react-moralis'
 
 function MyApp({ Component, pageProps }) {
-  console.log('process============', process.env.NEXT_PUBLIC_APP_ID)
   return (
     <MoralisProvider
       serverUrl={process.env.NEXT_PUBLIC_SERVER_URL}
